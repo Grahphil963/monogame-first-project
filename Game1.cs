ffb2d28c156d9a6c0a7bc977d08ecea132e9fe37
Philip Graham <GrahPhil963@GoTVDSB.ca>
2022-05-17 10:51:14 -0400

    work

--- Game1.cs
+++ Game1.cs
@@ -9,6 +9,18 @@ namespace monogame_first_project
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
         Texture2D dinoTexture;
+        Texture2D barbarianTexture;
+        Texture2D goblinTexture;
+        Texture2D giantTexture;
+        Texture2D AlCaponeTexture;
+        Texture2D cellTexture;
+
+
+        
+
+
+
+
 
 
         public Game1()
@@ -35,6 +47,18 @@ namespace monogame_first_project
 
             // TODO: use this.Content to load your game content here
             dinoTexture = Content.Load<Texture2D>("dino");
+            barbarianTexture = Content.Load<Texture2D>("barbarian");
+            goblinTexture = Content.Load<Texture2D>("Goblin");
+            giantTexture = Content.Load<Texture2D>("Giant");
+            AlCaponeTexture = Content.Load<Texture2D>("Al");
+            cellTexture = Content.Load<Texture2D>("Cell");
+            
+
+          
+            
+
+            
+            
             
         }
 
@@ -51,11 +75,19 @@ namespace monogame_first_project
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.IndianRed);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
             _spriteBatch.Begin();
-            _spriteBatch.Draw(dinoTexture, new Vector2(300, 30), Color.Red);
+            //_spriteBatch.Draw(dinoTexture, new Vector2(300, 30), Color.Red);
+            _spriteBatch.Draw(cellTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(barbarianTexture, new Vector2(100, 30), Color.White);
+            _spriteBatch.Draw(goblinTexture, new Vector2(400, 30), Color.White);
+            _spriteBatch.Draw(giantTexture, new Vector2(100, 300), Color.White);
+            _spriteBatch.Draw(AlCaponeTexture, new Vector2(400, 300), Color.White);
+            
+            
+
 
 
             _spriteBatch.End();
